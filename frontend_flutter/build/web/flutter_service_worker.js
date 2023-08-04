f0a41f1596c632fbb2bc81745da2911a8d81aac9
dev-dtplus <dev@dt.plus>
2023-08-04 16:38:22 +0900

    add meta client id in idex.html

--- frontend_flutter/build/web/flutter_service_worker.js
+++ frontend_flutter/build/web/flutter_service_worker.js
@@ -23,8 +23,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a548d6659fc28b540e08c69d72196cc9",
-"/": "a548d6659fc28b540e08c69d72196cc9",
+"index.html": "33ef5e1b7e9234b897fb77e880d203c8",
+"/": "33ef5e1b7e9234b897fb77e880d203c8",
 "main.dart.js": "91d0800573b07b755dd16b28a78d9771",
 "manifest.json": "70551d46d7f00097516978c67d565df3",
 "version.json": "66ea5ac640d1a59c14f71dd18177469a"};
