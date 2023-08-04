318fcbd42ddeb00e6b892a63126caaf720e46b66
dev-dtplus <dev@dt.plus>
2023-08-04 11:22:28 +0900

    remove fluor

--- frontend_flutter/build/web/flutter_service_worker.js
+++ frontend_flutter/build/web/flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "cceebdd33633d5cba24c44757b357791",
-"/": "cceebdd33633d5cba24c44757b357791",
-"main.dart.js": "280402c536f4836503aff7ade15cc298",
+"index.html": "26bbb7780edfa2f161394add04c959c6",
+"/": "26bbb7780edfa2f161394add04c959c6",
+"main.dart.js": "cbfc2ea22aa681bce6748f9bf99f7263",
 "manifest.json": "70551d46d7f00097516978c67d565df3",
 "version.json": "66ea5ac640d1a59c14f71dd18177469a"};
 // The application shell files that are downloaded before a service worker can
