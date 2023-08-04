7aa783bb4ff2fce696a7c59ba9a75e1c3be1dcd6
dev-dtplus <dev@dt.plus>
2023-08-04 12:29:24 +0900

    get token?

--- frontend_flutter/build/web/flutter_service_worker.js
+++ frontend_flutter/build/web/flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f8f2e8631425e5d4b1db5f87e2dbbe84",
-"/": "f8f2e8631425e5d4b1db5f87e2dbbe84",
-"main.dart.js": "6686d099d72d80d2fdf220acf6695dd7",
+"index.html": "2824198f56643c9caf7f8dcbb25d8663",
+"/": "2824198f56643c9caf7f8dcbb25d8663",
+"main.dart.js": "6918392dffb0ff53beae30a50b6b8b07",
 "manifest.json": "70551d46d7f00097516978c67d565df3",
 "version.json": "66ea5ac640d1a59c14f71dd18177469a"};
 // The application shell files that are downloaded before a service worker can
